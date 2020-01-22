915b55ce126e3bfd1944a14bd4141abd6b080218
Rodrigo Tozaki <digohito@gmail.com>
2020-01-22 02:19:55 -0200

    Troca da letra i pela k

--- Sum_list.py
+++ Sum_list.py
@@ -1,5 +1,7 @@
 def somaLista (lista):
     soma = 0
-    for i in range (0, len(lista)):
-        soma = soma + lista[i]
+    for k in range (0, len(lista)):
+        soma = soma + lista[k]
     return soma
+
+
